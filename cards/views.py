fe9275d0d6c7a5ca90c73430f2c7a5107ba389cb
Wes <wesbuck@gmail.com>
2018-10-21 20:10:22 -0400

    Disabled Update & Delete functionality for cards.

--- cards/views.py
+++ cards/views.py
@@ -1,10 +1,11 @@
 from cards.models import Card
 from cards.serializers import CardSerializer
 from cards.filters import CardFilter
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 
 
-class CardList(viewsets.ModelViewSet):
+# only extend the parts of ModelViewSet we want (omit Update & Destroy)
+class CardList(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = CardSerializer
     filter_class = CardFilter
     queryset = Card.objects.all()
