c5b12cce614b2a68fe31d85df3d0f21397d507df
Wes Buck <wesbuck@gmail.com>
2019-12-30 20:18:10 -0500

    added is_kingdom_card field and data for 2 sets

--- cards/models.py
+++ cards/models.py
@@ -8,6 +8,7 @@ class Card(models.Model):
     set_num = models.IntegerField(null=True)
     set_name = models.CharField(max_length=32, null=True)
     type = models.CharField(max_length=64, null=True)
+    is_kingdom_card = models.BooleanField(default=1)
     cost = models.CharField(max_length=4, null=True)
     card_text = models.TextField(max_length=1024, null=True)
 
