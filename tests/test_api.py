44dcaf084960ddf999f4e9b89567a3132135a9ce
Wes Buck <wes.buck@vidyard.com>
2020-10-03 16:09:21 -0400

    added and updated tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -32,8 +32,8 @@ def get_or_create_token(db, create_user):
    user = create_user()
    token, _ = Token.objects.get_or_create(user=user)
    return token
-   
-# Test for 401 Unauthorized Error when no Token is not present
+
+# Test for 401 Unauthorized Error when no Token is present
 @pytest.mark.django_db
 def test_unauthed_request(api_client):
    url = reverse('Random')
@@ -50,6 +50,16 @@ def test_random_request(api_client, get_or_create_token):
    assert response.status_code == 200
    assert len(response.data) == 9
 
+# Test that Kingdom gets a single card and returns properly
+@pytest.mark.django_db
+def test_kingdom_request(api_client, get_or_create_token):
+   url = reverse('Kingdom')
+   token = get_or_create_token
+   api_client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+   response = api_client.get(url)
+   assert response.status_code == 200
+   assert len(response.data) == 9
+
 # Test that CardSet gets 10 Cards
 @pytest.mark.django_db
 def test_cardset_request(api_client, get_or_create_token):
@@ -59,7 +69,17 @@ def test_cardset_request(api_client, get_or_create_token):
    response = api_client.get(url)
    assert response.status_code == 200
    assert len(response.data) == 10
-   
+
+# Test that All gets all Cards
+@pytest.mark.django_db
+def test_all_request(api_client, get_or_create_token):
+   url = reverse('All')
+   token = get_or_create_token
+   api_client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+   response = api_client.get(url)
+   assert response.status_code == 200
+   assert len(response.data) >= 360
+
 # Test that Card/[id] gets the correct stuff
 @pytest.mark.django_db
 def test_specific_card_request(api_client, get_or_create_token):
@@ -78,7 +98,7 @@ def test_card_creation(api_client, get_or_create_token):
    new_card = { 'uuid': '82f9bcc1-9ab9-4856-b04f-aace09668e21', 'card_name': 'New Card', 'set_num': 0, 'set_name': 'Promo', 'type': 'Action', 'cost': '$3', 'card_text': 'Sample Card Text'}
    token = get_or_create_token
    api_client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
-   response = api_client.post('/cards/')
+   response = api_client.post('/cards/', new_card)
    print(response.data)
    assert response.status_code == 201
    assert len(response.data) == 9
