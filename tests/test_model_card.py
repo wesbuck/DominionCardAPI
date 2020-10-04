8606dc98b15dc0aa932b24c26819c5dbffbf3ac2
Wes Buck <wes.buck@vidyard.com>
2020-10-03 23:56:29 -0400

    add model test

---
+++ tests/test_model_card.py
@@ -0,0 +1,12 @@
+import uuid
+import pytest
+from cards.models import Card
+
+@pytest.mark.django_db
+def test_card_create_and_str():
+    test_card_data = { 'uuid': uuid.uuid4(), 'card_name': 'Test Card', 'set_num': 0, 'set_name': 'Promo', 'type': 'Action', 'is_kingdom_card': 1, 'cost': '$3', 'card_text': 'Sample Card Text'}
+    test_card = Card()
+    for field in test_card_data:
+        setattr(test_card, field, test_card_data[field])
+    test_card.save()
+    assert test_card_data['card_name'] == str(test_card)
