17ad63a0ee1150f4a18bebc8186ca6da1998a42e
Wes Buck <wes.buck@vidyard.com>
2020-10-03 23:08:55 -0400

    cleanup

--- cards/models.py
+++ cards/models.py
@@ -5,7 +5,7 @@ import uuid
 class Card(models.Model):
     uuid = models.CharField(max_length=64, null=True, default=uuid.uuid4, unique=True, error_messages={
         'unique': 'The uuid already exists'
-    }) #, editable=False)
+    })
     card_name = models.CharField(max_length=64, null=False, unique=True, error_messages={
         'unique': 'The card name already exists'
     })
